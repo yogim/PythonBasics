0c9a9c71fb76188c0878afaa19d19dd212823b96
yogim <yogeshmhase89@gmail.com>
2017-10-07 15:46:31 -0700

    number and operation basics

---
+++ numberoperationbasics.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  7 15:41:11 2017
+
+@author: Yogesh
+"""
+
+a=2 
+b=3
+
+
+# multipliction
+print(a*b)
+ 
+ # division
+print(a/b)
+ 
+ 
+ #floor division - ignore number after decimal
+print (a//b)
+ 
+ #print remiander
+print(a%b)
+
+
+# power to a number
+# 2 ** 4 = 2^4
+print(2**4)
+
+ 
+ 
