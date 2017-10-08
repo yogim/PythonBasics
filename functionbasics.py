467c60ce542c4f5101aa13f7ecbd97360202b2b2
yogim <yogeshmhase89@gmail.com>
2017-10-07 17:45:41 -0700

    coorection in function basics

--- functionbasics.py
+++ functionbasics.py
@@ -5,10 +5,43 @@ Created on Sat Oct  7 17:33:56 2017
 @author: Yogesh
 """
 
+# simple function
+
+def hello():
+    print('hello')
+
+hello()
 
 
-def hello(a):
-    print('hello world'+a    )
 
-    
+#Passing argument
+
+def hello(name):
+    print('hello,'+name)
+
 hello("yogesh")
+
+
+
+# default argument
+def hello(name = "Everyone"):
+    print('hello ,'+name)
+
+hello("yogesh")
+hello()
+
+
+#keyowrd argument
+def hello(name = "Everyone"):
+    print('hello ,'+name)
+hello(name = "yogesh")
+
+
+#Lamba expression
+def addition(n):
+    return lambda x : x+n
+ 
+fun = addition(2)
+print(fun(3))
+
+
